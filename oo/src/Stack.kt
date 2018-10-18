c744543c3b47bd5e7c35c2a1aa5f577dc29c8b39
Carmaikol <mikizorzo@gmail.com>
2018-10-18 12:29:48 +0200

    Object Oriented Stack Skeleton

--- oo/src/Stack.kt
+++ oo/src/Stack.kt
@@ -2,17 +2,37 @@
 // Keep your code DRY    DONT REPEAT YOURSELF
 
 
-class Stack<T> {
+class Stack<T>(vararg val items : T) {
 
-    fun push() {
-
-
-    }
+    val elements = items.toMutableList()
 
 
-    fun pop() {
-
+    fun push(element : T) {
+       elements.add(element)
 
     }
+
+
+    fun pop(): T? {
+        if(!isEmpty()) {
+            return elements.removeAt(elements.size - 1)
+        }
+        return null
+    }
+
+    private fun isEmpty() : Boolean {
+        return elements.isEmpty()
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    val stack = Stack(3,5,2,8)
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
 
 }
