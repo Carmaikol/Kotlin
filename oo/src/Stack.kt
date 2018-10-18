0df6047c7044713e9a2c6f4c180400c094bc5699
Carmaikol <mikizorzo@gmail.com>
2018-10-18 12:58:08 +0200

    Object Oriented
    Number Guessing
    Hangman

--- oo/src/Stack.kt
+++ oo/src/Stack.kt
@@ -23,12 +23,22 @@ class Stack<T>(vararg val items : T) {
     private fun isEmpty() : Boolean {
         return elements.isEmpty()
     }
+}
 
+fun <T> stackOf(vararg elements: T) : Stack<T>{
+    return Stack(*elements)
 }
 
 
 fun main(args: Array<String>) {
     val stack = Stack(3,5,2,8)
+
+    val stack2 = stackOf("Hi","Hey","hello")
+
+    for (i in 1..3) {
+        println(stack2.pop())
+    }
+
     println(stack.pop())
     println(stack.pop())
     println(stack.pop())
